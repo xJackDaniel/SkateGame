0cbf1cc88c2a1d1b58a6ea412bd005d12a8ef767
avigailbarkan <avigailbarkan@gmail.com>
2022-03-24 18:49:17 +0200

    classes

---
+++ classes/Bar.py
@@ -0,0 +1,18 @@
+class Bar:
+    def __init__(self, type, height, width, jump):
+        self.type = type
+        self.height = height
+        self.width = width
+        self.jump = jump
+
+    def get_type(self):
+        return self.type
+
+    def get_height(self):
+        return self.height
+
+    def get_width(self):
+        return self.width
+
+    def get_jump(self):
+        return self.jump
